947809a7b7e296e78f2720e9fff21a170b92ee1e
Team <team@fastcode.com.br>
2022-07-12 14:58:23 -0300

    Jest aceitando importações absolutas

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,20 @@
-const Main = () => {
+import * as S from './styles'
+
+export const Main = () => {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        Typescript, React.js, Next.js, e Styled Components
+      </S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para uma tela com código."
+      />
+    </S.Wrapper>
   )
 }
-
-export default Main
